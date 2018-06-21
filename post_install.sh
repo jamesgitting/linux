8a5927e5108349bb76aaa01948fbf2949bf3eb70
James Gitting <james.gitting@gmail.com>
2018-06-21 09:38:13 -0700

    Fixing newline

--- post_install.sh
+++ post_install.sh
@@ -38,7 +38,7 @@ apt-get install -y guake
 # VS Code
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" >
+sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > \
  /etc/apt/sources.list.d/vscode.list'
 
 # Atom
