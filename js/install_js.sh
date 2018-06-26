e99ab1add13972f5a1239abdf71c65998365a7e5
James Gitting <james.gitting@gmail.com>
2018-06-26 12:34:54 -0700

    npm install directories

--- js/install_js.sh
+++ js/install_js.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 ###################################################################################################
 #                                                                                                 #
 #                               Install Script for JS Development                                 #
@@ -7,3 +9,8 @@
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 
 apt-get install -y nodejs
+
+mkdir ~/.npm-global
+npm config set prefix '~/.npm-global'
+export PATH=~/.npm-global/bin:$PATH
+source ~/.profile
